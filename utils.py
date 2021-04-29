4db99ffb9b34fe1d75b43187d7d43fc9777d45ff
liangXD523 <dongxuliang18@163.com>
2021-04-29 16:25:10 -0400

    adding graph drawing functions

--- utils.py
+++ utils.py
@@ -26,7 +26,7 @@ import json
 def save_graph_list(G_list, fname, clean=False, has_par=False, nodes_par1_list=None, nodes_par2_list=None):
     with open(fname, "wb") as f:
         graphs_info = []
-        for i,G in enumerate(G_list):
+        for i, G in enumerate(G_list):
             if clean:
                 G = max(nx.connected_component_subgraphs(G), key=len)
             if has_par:
@@ -34,6 +34,7 @@ def save_graph_list(G_list, fname, clean=False, has_par=False, nodes_par1_list=N
             else:
                 graphs_info.append([G.nodes(), G.edges()])
         pickle.dump(graphs_info, f)
+
 
 def load_graph_list(fname, has_par=False):
     with open(fname, "rb") as f:
@@ -57,38 +58,94 @@ def load_graph_list(fname, has_par=False):
 
 
 # draw a list of graphs [G]
-def draw_graph_list(G_list, row, col, fname = 'figures/test', layout='spring', is_single=False,k=1,node_size=55,alpha=1,width=1.3):
+def draw_graph_list(G_list,
+                    row,
+                    col,
+                    fname='figures/test',
+                    layout='spring',
+                    is_single=False,
+                    k=1,
+                    node_size=55,
+                    alpha=1,
+                    width=1.3):
     # # draw graph view
     # from pylab import rcParams
     # rcParams['figure.figsize'] = 12,3
-    if len(G_list)>row*col:
-        G_list = G_list[:row*col]
+    if len(G_list) > row * col:
+        G_list = G_list[:row * col]
     plt.switch_backend('agg')
-    for i,G in enumerate(G_list):
-        plt.subplot(row,col,i+1)
-        plt.subplots_adjust(left=0, bottom=0, right=1, top=1,
-                        wspace=0, hspace=0)
+    for i, G in enumerate(G_list):
+        plt.subplot(row, col, i + 1)
+        plt.subplots_adjust(left=0, bottom=0, right=1, top=1, wspace=0, hspace=0)
         plt.axis("off")
-        if layout=='spring':
-            pos = nx.spring_layout(G,k=k/np.sqrt(G.number_of_nodes()),iterations=20) # default 100
+        if layout == 'spring':
+            pos = nx.spring_layout(G, k=k / np.sqrt(G.number_of_nodes()), iterations=20)  # default 100
             # pos = nx.spring_layout(G)
 
-        elif layout=='spectral':
+        elif layout == 'spectral':
             pos = nx.spectral_layout(G)
         # # nx.draw_networkx(G, with_labels=True, node_size=2, width=0.15, font_size = 1.5, node_color=colors,pos=pos)
         # nx.draw_networkx(G, with_labels=False, node_size=1.5, width=0.2, font_size = 1.5, linewidths=0.2, node_color = 'k',pos=pos,alpha=0.2)
 
         if is_single:
             # node_size default 60, edge_width default 1.5
-            nx.draw_networkx_nodes(G, pos, node_size=node_size, node_color='#336699', alpha=1, linewidths=0, font_size=0)
+            nx.draw_networkx_nodes(
+                G, pos, node_size=node_size, node_color='#336699', alpha=1, linewidths=0, font_size=0)
             nx.draw_networkx_edges(G, pos, alpha=alpha, width=width)
         else:
-            nx.draw_networkx_nodes(G, pos, node_size=1.5, node_color='#336699',alpha=1, linewidths=0.2, font_size = 1.5)
-            nx.draw_networkx_edges(G, pos, alpha=0.3,width=0.2)
+            nx.draw_networkx_nodes(G, pos, node_size=1.5, node_color='#336699', alpha=1, linewidths=0.2, font_size=1.5)
+            nx.draw_networkx_edges(G, pos, alpha=0.3, width=0.2)
 
     plt.tight_layout()
-    plt.savefig(fname+'.png', dpi=600)
+    plt.savefig(fname + '.png', dpi=600)
     plt.close()
+
+# added by liang
+def my_draw_graph_list_separate(G_list,
+                                fname='figures/test',
+                                layout='spring',
+                                is_single=False,
+                                k=1,
+                                node_size=55,
+                                alpha=1,
+                                width=1.3):
+
+    for i, G in enumerate(G_list):
+        print('i =', i)
+        plt.switch_backend('agg')
+
+        plt.axis("off")
+
+        # turn off axis label
+        # plt.xticks([])
+        # plt.yticks([])
+
+        if layout == 'spring':
+            pos = nx.spring_layout(G, k=k / np.sqrt(G.number_of_nodes()), iterations=100)
+        elif layout == 'spectral':
+            pos = nx.spectral_layout(G)
+
+        if is_single:
+            # node_size default 60, edge_width default 1.5
+            nx.draw_networkx_nodes(
+                G, pos, node_size=node_size, node_color='#336699', alpha=1, linewidths=0)  # font_size=0
+            nx.draw_networkx_edges(G, pos, alpha=alpha, width=width)
+            '========XD========'
+            # print('G.nodes:', G.nodes())
+            # node_colors = [type_to_color[rm_idx(n)] for n in G.nodes()]
+            # print('node_colors:', node_colors)
+            # pos = nx.spring_layout(G)
+            # nx.draw(G, pos, with_labels=True, node_color=node_colors, node_size=200, font_size=10)
+            '========XD========'
+        else:
+            nx.draw_networkx_nodes(G, pos, node_size=1.5, node_color='#336699', alpha=1, linewidths=0.2, font_size=1.5)
+            nx.draw_networkx_edges(G, pos, alpha=0.3, width=0.2)
+
+        plt.draw()
+        plt.tight_layout()
+        plt.savefig(fname + '_{:03d}.png'.format(i), dpi=600)
+        plt.close()
+
 
 if __name__ == '__main__':
     configs_file = '/Users/jiaxuan/Downloads/best_configs.json'
@@ -96,7 +153,7 @@ if __name__ == '__main__':
         configs = json.load(f)
     pdb.set_trace()
     fname = 'GCN_3_32_preTrue_dropFalse_yield1_08000.dat'
-    graphs = load_graph_list('graphs/'+fname)
+    graphs = load_graph_list('graphs/' + fname)
     graph = graphs[0]
     pdb.set_trace()
-    draw_graph_list(graphs, row=4, col=4, fname='fig/'+fname)
+    draw_graph_list(graphs, row=4, col=4, fname='fig/' + fname)
