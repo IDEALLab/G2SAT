d286114d8fc7c36577401c18ba6401e2da7a77ee
liangXD523 <dongxuliang18@163.com>
2021-04-29 16:28:38 -0400

    changing data loading into our derectories

--- main_test.py
+++ main_test.py
@@ -5,13 +5,12 @@ from data import *
 from utils import *
 from tensorboardX import SummaryWriter
 
-
 ### args
 args = make_args()
 print(args)
 np.random.seed(123)
-args.name = '{}_{}_{}_pre{}_drop{}_yield{}_{}'.format(
-    args.model, args.layer_num, args.hidden_dim, args.feature_pre, args.dropout, args.yield_prob, args.comment)
+args.name = '{}_{}_{}_pre{}_drop{}_yield{}_{}'.format(args.model, args.layer_num, args.hidden_dim, args.feature_pre,
+                                                      args.dropout, args.yield_prob, args.comment)
 
 ### set up gpu
 if args.gpu:
@@ -20,7 +19,7 @@ if args.gpu:
     print('Using GPU {}'.format(os.environ['CUDA_VISIBLE_DEVICES']))
 else:
     print('Using CPU')
-device = torch.device('cuda:'+str(args.cuda) if args.gpu else 'cpu')
+device = torch.device('cuda:' + str(args.cuda) if args.gpu else 'cpu')
 
 ### create graph templates to initailize generator
 if not os.path.isdir('graphs/'):
@@ -28,33 +27,42 @@ if not os.path.isdir('graphs/'):
 template_name = 'graphs/template_small.dat'
 train_name = 'graphs/train_small.dat'
 
+print('args.recompute_template:', args.recompute_template)
+# args.recompute_template = True  # use this line to recompute for circuit stats. Not sure why had to do this...
 if args.recompute_template or not os.path.isfile(template_name):
-    graphs_train, nodes_par1s_train, nodes_par2s_train = load_graphs_lcg(data_dir='dataset/train_set/',
-                                                                         stats_dir='dataset/')
-    save_graph_list(graphs_train,train_name, has_par=True,nodes_par1_list=nodes_par1s_train,nodes_par2_list=nodes_par2s_train)
+    # graphs_train, nodes_par1s_train, nodes_par2s_train = load_graphs_lcg(
+    #     data_dir='dataset/train_set/', stats_dir='dataset/')
+    graphs_train, nodes_par1s_train, nodes_par2s_train = load_graphs_lcg_LENS(
+        data_dir='dataset_LENS/train_set/', stats_dir='dataset_LENS/')
+    save_graph_list(
+        graphs_train, train_name, has_par=True, nodes_par1_list=nodes_par1s_train, nodes_par2_list=nodes_par2s_train)
     print('Train graphs saved!', len(graphs_train))
     node_nums = [graph.number_of_nodes() for graph in graphs_train]
     edge_nums = [graph.number_of_edges() for graph in graphs_train]
     print('Num {}, Node {} {} {}, Edge {} {} {}'.format(
-        len(graphs_train), min(node_nums), max(node_nums), sum(node_nums) / len(node_nums), min(edge_nums),
-        max(edge_nums), sum(edge_nums) / len(edge_nums)))
+        len(graphs_train), min(node_nums), max(node_nums),
+        sum(node_nums) / len(node_nums), min(edge_nums), max(edge_nums),
+        sum(edge_nums) / len(edge_nums)))
 
-    dataset_train = Dataset_sat(graphs_train, nodes_par1s_train, nodes_par2s_train, epoch_len=5000,
-                                yield_prob=args.yield_prob, speedup=False)
+    dataset_train = Dataset_sat(
+        graphs_train, nodes_par1s_train, nodes_par2s_train, epoch_len=5000, yield_prob=args.yield_prob, speedup=False)
 
     graph_templates, nodes_par1s, nodes_par2s = dataset_train.get_template_fast()
-    save_graph_list(graph_templates,template_name, has_par=True,nodes_par1_list=nodes_par1s,nodes_par2_list=nodes_par2s)
+    save_graph_list(
+        graph_templates, template_name, has_par=True, nodes_par1_list=nodes_par1s, nodes_par2_list=nodes_par2s)
     print('Template saved!')
 else:
     graph_templates, nodes_par1s, nodes_par2s = load_graph_list(template_name, has_par=True)
     print('Template loaded!')
+    # print(len(nodes_par1s), len(nodes_par2s))  # 24, 24
 
+print(graph_templates[0].number_of_nodes())
 
 print('Template num', len(graph_templates))
 
-
 # load stats
-with open('dataset/' + 'lcg_stats.csv') as csvfile:
+# with open('dataset/' + 'lcg_stats.csv') as csvfile:
+with open('dataset_LENS/' + 'circuit_stats.csv') as csvfile:
     data = csv.reader(csvfile, delimiter=',')
     stats = []
     for stat in data:
@@ -63,25 +71,31 @@ generator_list = []
 for i in range(len(graph_templates)):
     # find clause num
     for stat in stats:
-        if int(stat[1]) == len(nodes_par1s[i])//2:
-            clause_num = int(int(stat[2])*args.clause_ratio)
+        if int(stat[1]) == len(nodes_par1s[i]) // 2:
+            clause_num = int(int(stat[2]) * args.clause_ratio)
             break
     for j in range(args.gen_graph_num):
-        generator_list.append(graph_generator(
-            graph_templates[i], len(nodes_par1s[i]), sample_size=args.sample_size, device=device, clause_num=clause_num))
+        generator_list.append(
+            graph_generator(
+                graph_templates[i],
+                len(nodes_par1s[i]),
+                sample_size=args.sample_size,
+                device=device,
+                clause_num=clause_num))
 
-
-input_dim = 3 # 3 node types
-model = locals()[args.model](input_dim=input_dim, feature_dim=args.feature_dim,
-                hidden_dim=args.hidden_dim, output_dim=args.output_dim,
-                feature_pre=args.feature_pre, layer_num=args.layer_num, dropout=args.dropout).to(device)
-model_fname = 'model/'+args.name+str(args.epoch_load)
+input_dim = 3  # 3 node types
+model = locals()[args.model](
+    input_dim=input_dim,
+    feature_dim=args.feature_dim,
+    hidden_dim=args.hidden_dim,
+    output_dim=args.output_dim,
+    feature_pre=args.feature_pre,
+    layer_num=args.layer_num,
+    dropout=args.dropout).to(device)
+model_fname = 'model/' + args.name + str(args.epoch_load)
 model.load_state_dict(torch.load(model_fname, map_location=device))
 model.to(device)
 model.eval()
 print('Model loaded!', model_fname)
 
 test(args, generator_list, model, repeat=args.repeat)
-
-
-
--- train.py
+++ train.py
@@ -29,9 +29,7 @@ from data import *
 import logging
 
 
-
-def train(args, loader_train, loader_test, model, optimizer,
-          writer_train, writer_test, device, save_dir='model/'):
+def train(args, loader_train, loader_test, model, optimizer, writer_train, writer_test, device, save_dir='model/'):
     if not os.path.isdir(save_dir):
         os.mkdir(save_dir)
     loss_func = nn.BCEWithLogitsLoss()
@@ -61,8 +59,14 @@ def train(args, loader_train, loader_test, model, optimizer,
             nodes_first = torch.index_select(out, 0, edge_mask[0, :].long().to(device))
             nodes_second = torch.index_select(out, 0, edge_mask[1, :].long().to(device))
             pred = torch.sum(nodes_first * nodes_second, dim=-1)
-            label_positive = torch.ones([data.node_index_positive.shape[1], ], dtype=pred.dtype)
-            label_negative = torch.zeros([data.node_index_negative.shape[1], ], dtype=pred.dtype)
+            label_positive = torch.ones(
+                [
+                    data.node_index_positive.shape[1],
+                ], dtype=pred.dtype)
+            label_negative = torch.zeros(
+                [
+                    data.node_index_negative.shape[1],
+                ], dtype=pred.dtype)
             label = torch.cat((label_positive, label_negative)).to(device)
             loss = loss_func(pred, label)
             loss_train += loss.cpu().data.numpy()
@@ -72,7 +76,7 @@ def train(args, loader_train, loader_test, model, optimizer,
             pred_np_max += pred_np.max()
             pred_np_mean += pred_np.mean()
             auc_train += roc_auc_score(label_np, pred_np)
-            acc_train += np.mean((pred_np>0.5).astype(int)==label_np)
+            acc_train += np.mean((pred_np > 0.5).astype(int) == label_np)
             # update
             loss.backward()
             optimizer.step()
@@ -83,13 +87,13 @@ def train(args, loader_train, loader_test, model, optimizer,
                     grad_norms.append(p.grad.norm().cpu().numpy())
             grad_norm_min += min(grad_norms)
             grad_norm_max += max(grad_norms)
-            grad_norm_mean += sum(grad_norms)/len(grad_norms)
+            grad_norm_mean += sum(grad_norms) / len(grad_norms)
             counter += 1
             time2 = time.time()
-            time_model += time2-time1
+            time_model += time2 - time1
         time_total = time.time() - time_total
         if epoch % args.epoch_log == 0:
-            print('Train time per epoch: total {:.4f}, model {:.4f}'.format(time_total,time_model))
+            print('Train time per epoch: total {:.4f}, model {:.4f}'.format(time_total, time_model))
         loss_train /= counter
         auc_train /= counter
         acc_train /= counter
@@ -109,10 +113,10 @@ def train(args, loader_train, loader_test, model, optimizer,
         writer_train.add_scalar('pred_np_mean', pred_np_mean, epoch)
 
         if epoch % args.epoch_save == 0:
-            torch.save(model.state_dict(), save_dir+args.name+str(epoch))
+            torch.save(model.state_dict(), save_dir + args.name + str(epoch))
             print('model saved!')
 
-        if epoch % args.epoch_log == 0 and epoch>=args.epoch_test:
+        if epoch % args.epoch_log == 0 and epoch >= args.epoch_test:
             # test
             loss_test = 0
             auc_test = 0
@@ -128,8 +132,14 @@ def train(args, loader_train, loader_test, model, optimizer,
                 nodes_first = torch.index_select(out, 0, edge_mask[0, :].long().to(device))
                 nodes_second = torch.index_select(out, 0, edge_mask[1, :].long().to(device))
                 pred = torch.sum(nodes_first * nodes_second, dim=-1)
-                label_positive = torch.ones([data.node_index_positive.shape[1], ], dtype=pred.dtype)
-                label_negative = torch.zeros([data.node_index_negative.shape[1], ], dtype=pred.dtype)
+                label_positive = torch.ones(
+                    [
+                        data.node_index_positive.shape[1],
+                    ], dtype=pred.dtype)
+                label_negative = torch.zeros(
+                    [
+                        data.node_index_negative.shape[1],
+                    ], dtype=pred.dtype)
                 label = torch.cat((label_positive, label_negative)).to(device)
                 loss_test += loss_func(pred, label).cpu().data.numpy()
                 label_np = label.flatten().cpu().numpy()
@@ -142,7 +152,7 @@ def train(args, loader_train, loader_test, model, optimizer,
             acc_test /= counter
             print('Model {}'.format(args.model), epoch, 'Loss {:.4f}'.format(loss_train),
                   'Train AUC: {:.4f}'.format(auc_train), 'Test AUC: {:.4f}'.format(auc_test),
-                'Train ACC: {:.4f}'.format(acc_train), 'Test ACC: {:.4f}'.format(acc_test))
+                  'Train ACC: {:.4f}'.format(acc_train), 'Test ACC: {:.4f}'.format(acc_test))
 
             writer_test.add_scalar('auc', auc_test, epoch)
             writer_test.add_scalar('loss', loss_test, epoch)
@@ -150,47 +160,67 @@ def train(args, loader_train, loader_test, model, optimizer,
     return model
 
 
-
-
-
 def test(args, generator_list, model, repeat=0, outdir='graphs/'):
     if not os.path.isdir(outdir):
         os.mkdir(outdir)
     # generate graph batch
-    for i,generator in enumerate(generator_list):
+    generator_list = generator_list[:20]  # TODO: gonna make this 20 not hard-coded
+    for i, generator in enumerate(generator_list):  
         time0 = time.time()
         time_model = 0
         generator.reset()
         while True:
+            # print('-' * 10)
             time1 = time.time()
             out = model(generator.data)
+            # print('out:', out.shape, out)  # shape = (45, 32)
+            # print('generator.data.node_index:', generator.data.node_index.shape,
+            #       generator.data.node_index)  # shape = (2, x). E.g. (2, 5)
+            # TODO: what is the original G2SAT behavior here? Why does LENS data lead to a (2, 0) gene.data.node_index?
+
             nodes_first = torch.index_select(out, 0, generator.data.node_index[0, :])
             nodes_second = torch.index_select(out, 0, generator.data.node_index[1, :])
+            # print(nodes_first.shape, nodes_second.shape)  # E.g. (5, 32)
             pred = torch.sum(nodes_first * nodes_second, dim=-1)
+            # print('pred', pred)
             pred_id = torch.argmax(pred).data
             time2 = time.time()
-            time_model += time2-time1
+            time_model += time2 - time1
             exit_flag = generator.update(generator.data.node_index[:, pred_id])
+            # print('number of nodes:', generator.graph.number_of_nodes())
+
             if exit_flag:
                 break
+            # added by liang. Need investigation here. See the TODO above.
+            if generator.data.node_index.shape[1] == 0:
+                break
+            # if generator.graph.number_of_nodes() >= 20:
+            #     print('break', generator.graph.number_of_nodes())
+            #     break
+
         time3 = time.time()
-        print('Generate time for 1 graph: total {:.4f}, model {:.4f}'.format(
-            time3 - time0, time_model))
-        logging.info('Generate time for 1 graph: total {:.4f}, model {:.4f}'.format(
-            time3 - time0, time_model))
-        outdir_single_graph = outdir + args.name + '_' + str(args.epoch_load) + '_' + str(repeat) + '/'
-        if not os.path.isdir(outdir_single_graph):
-            os.mkdir(outdir_single_graph)
-        save_graph_list([generator.graph],  outdir_single_graph + str(i) + '.dat')
+        print('Generate time for 1 graph: total {:.4f}, model {:.4f}'.format(time3 - time0, time_model))
+        logging.info('Generate time for 1 graph: total {:.4f}, model {:.4f}'.format(time3 - time0, time_model))
+
+        # exit()
+
+        # outdir_single_graph = outdir + args.name + '_' + str(args.epoch_load) + '_' + str(repeat) + '/'
+        # if not os.path.isdir(outdir_single_graph):
+        #     os.mkdir(outdir_single_graph)
+        # save_graph_list([generator.graph],  outdir_single_graph + str(i) + '.dat')
 
     graphs = [generator.graph for generator in generator_list]
-    save_graph_list(graphs, outdir + args.name+'_'+str(args.epoch_load)+'_'+str(repeat) + '.dat')
+
+    my_draw_graph_list_separate(graphs, is_single=True)  # todo: add bipartite color for visualization
+
+    # save_graph_list(graphs, outdir + args.name+'_'+str(args.epoch_load)+'_'+str(repeat) + '.dat')
     node_nums = [graph.number_of_nodes() for graph in graphs]
     edge_nums = [graph.number_of_edges() for graph in graphs]
     print('Num {}, Node {} {} {}, Edge {} {} {}'.format(
-        len(graphs), min(node_nums), max(node_nums), sum(node_nums) / len(node_nums), min(edge_nums),
-        max(edge_nums), sum(edge_nums) / len(edge_nums)))
+        len(graphs), min(node_nums), max(node_nums),
+        sum(node_nums) / len(node_nums), min(edge_nums), max(edge_nums),
+        sum(edge_nums) / len(edge_nums)))
     logging.info('Num {}, Node {} {} {}, Edge {} {} {}'.format(
-        len(graphs), min(node_nums), max(node_nums), sum(node_nums) / len(node_nums), min(edge_nums),
-        max(edge_nums), sum(edge_nums) / len(edge_nums)))
-
+        len(graphs), min(node_nums), max(node_nums),
+        sum(node_nums) / len(node_nums), min(edge_nums), max(edge_nums),
+        sum(edge_nums) / len(edge_nums)))
